6d55bb590b8b6de883c3bbf38c92a3eca42b3444
jesp209i <jesp209i@edu.eal.dk>
2017-11-30 14:18:18 +0100

    Person klasse oprettet

---
+++ KoWorkers/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoWorkers
+{
+    public class Person
+    {
+        private string firstName;
+        public Person(string newFirstName)
+        {
+            firstName = newFirstName;
+        }
+        public string GetName()
+        {
+            return firstName;
+        }
+        public string SetName(string setFirstName)
+        {
+            firstName = setFirstName;
+        }
+    }
+}
