23a47e4d73534ecc6ac5be22ba83810661d2555a
jesp209i <jesp209i@edu.eal.dk>
2017-12-07 21:15:20 +0100

    edited choise to choice

--- KoWorkers/Menu.cs
+++ KoWorkers/Menu.cs
@@ -23,8 +23,8 @@ namespace KoWorkers
             Console.WriteLine();
             Console.WriteLine("0. Afslut");
 
-                string choise = GetUserChoise("Indtast valg:");
-                switch (choise)
+                string choice = GetUserChoice("Indtast valg:");
+                switch (choice)
                 {
                     case "1": StartEmployeeMenu(); break;
                     case "0": keepMenuRunning = false; break;
@@ -49,7 +49,7 @@ namespace KoWorkers
             Console.WriteLine();
             Console.WriteLine(employeeControl.employeeRepo.ListAllEmployees());
 
-                string choise = GetUserChoise("Indtast valg:");
+                string choise = GetUserChoice("Indtast valg:");
                 switch (choise)
                 {
                     case "1": NewEmployee(); break;
@@ -73,11 +73,11 @@ namespace KoWorkers
             string getNumberOfEmployee;
             do
             {
-                getNumberOfEmployee = GetUserChoise("indtast tal ud for navn");
+                getNumberOfEmployee = GetUserChoice("indtast tal ud for navn");
             } while (!int.TryParse(getNumberOfEmployee, out int s));
             Employee updateEmployee = employeeControl.GetEmployee(int.Parse(getNumberOfEmployee));
-            string newFirstName = GetUserChoise("Indtast nyt fornavn");
-            string newLastName = GetUserChoise("Indtast nyt efternavn");
+            string newFirstName = GetUserChoice("Indtast nyt fornavn");
+            string newLastName = GetUserChoice("Indtast nyt efternavn");
             updateEmployee.FirstName = newFirstName;
             updateEmployee.LastName = newLastName;
             employeeControl.UpdateEmployee(updateEmployee);
@@ -100,14 +100,14 @@ namespace KoWorkers
             string getNumberOfEmployee;
             do
             {
-                getNumberOfEmployee = GetUserChoise("Indtast tal ud for navn:");
+                getNumberOfEmployee = GetUserChoice("Indtast tal ud for navn:");
             } while (!int.TryParse(getNumberOfEmployee,out int s));
             Employee removeEmployee = employeeControl.GetEmployee(int.Parse(getNumberOfEmployee));
             bool hasNotChosenCorrectly = true;
             do
             {
                 Console.WriteLine("Er du sikker på at du vil slette " + removeEmployee.GetName());
-                string choise = GetUserChoise(" Skriv \"j\" for ja og \"n\" for nej");
+                string choise = GetUserChoice(" Skriv \"j\" for ja og \"n\" for nej");
                 switch (choise)
                 {
                     case "j":
@@ -137,9 +137,9 @@ namespace KoWorkers
             Console.WriteLine();
             Console.WriteLine("Vi skal bruge et fornavn og et efternavn.");
             Console.WriteLine("---------");
-            firstName = GetUserChoise("Først fornavn:");
+            firstName = GetUserChoice("Først fornavn:");
             Console.WriteLine();
-            lastName = GetUserChoise("Efternavn:");
+            lastName = GetUserChoice("Efternavn:");
             employeeControl.AddEmployee(firstName, lastName);
         }
 
@@ -152,7 +152,7 @@ namespace KoWorkers
         }
 
        
-        public string GetUserChoise(string text)
+        public string GetUserChoice(string text)
         {
             Console.WriteLine(text);
             string userInput = Console.ReadLine();
