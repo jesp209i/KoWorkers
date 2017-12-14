162846542cd453cb6c016679593b0e8bad7fb4c5
Lars Kjær Rasmussen <lars5030@edu.eal.dk>
2017-12-14 01:55:33 +0100

    Updateret med Remove

--- KoWorkerGui/Coworker_Window.xaml.cs
+++ KoWorkerGui/Coworker_Window.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using KoWorkers;
 
 namespace KoWorkerGui
 {
@@ -19,9 +20,11 @@ namespace KoWorkerGui
     /// </summary>
     public partial class Coworker_Window : Window
     {
+        EmployeeRepository ep;
         public Coworker_Window()
         {
             InitializeComponent();
+            ep = new EmployeeRepository();
         }
 
         private void AddEmployee_Click(object sender, RoutedEventArgs e)
@@ -34,7 +37,23 @@ namespace KoWorkerGui
 
         private void Back_Button_Click(object sender, RoutedEventArgs e)
         {
+            MainWindow Main = new MainWindow();
+            App.Current.MainWindow = Main;
+            this.Close();
+            Main.Show();
+        }
 
+        private void RemoveEmployee_Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            RemoveEmployee_Window Remove = new RemoveEmployee_Window();
+            App.Current.MainWindow = Remove;
+            foreach(Employee employee in ep.employees)
+            {
+                Remove.RemoveEmployees_ListBox.Items.Add(employee.EmployeeId + ". " + employee.GetName() + " Tlf: " + employee.GetTelephoneNO());
+            }
+            this.Close();
+            Remove.Show();        
         }
     }
 }
--- KoWorkerGui/RemoveEmployee_Window.xaml.cs
+++ KoWorkerGui/RemoveEmployee_Window.xaml.cs
@@ -11,17 +11,43 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using KoWorkers;
 
 namespace KoWorkerGui
 {
+    
     /// <summary>
     /// Interaction logic for RemoveEmployee_Window.xaml
     /// </summary>
     public partial class RemoveEmployee_Window : Window
     {
+        Controller C;
+        EmployeeRepository ep;
         public RemoveEmployee_Window()
         {
             InitializeComponent();
+            C = new Controller();
+            ep = new EmployeeRepository();
+        }
+
+        private void RemoveEmployee_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Employee removeEmplo = ep.employees[RemoveEmployees_ListBox.SelectedIndex];            
+            C.RemoveEmployee(removeEmplo);
+
+        }
+
+        private void Back_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Coworker_Window coworker_Window = new Coworker_Window();
+            App.Current.MainWindow = coworker_Window;
+            this.Close();
+            coworker_Window.Show();
+        }
+
+        private void RemoveEmployees_ListBox_SelectionChanced(object sender, SelectionChangedEventArgs e)
+        {
+            ShowSelected_Label.Content = ep.employees[RemoveEmployees_ListBox.SelectedIndex].FirstName;
         }
     }
 }
