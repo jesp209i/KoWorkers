796f9bdef91de4ea77a24efa82257aa9177e7aee
jesp209i <jesp209i@edu.eal.dk>
2018-02-23 16:44:55 +0100

    WorkSchedule added WorkScheduleRepo added WorkScheduleShift added some tests added

---
+++ KoWorkers/WorkSchedule/WorkScheduleRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoWorkers.WorkSchedule
+{
+    public class WorkScheduleRepository
+    {
+        private static WorkScheduleRepository instance = null;
+        private List<WorkSchedule> workSchedules;
+        private WorkScheduleRepository()
+        {
+            workSchedules = new List<WorkSchedule>();
+        }
+        public static WorkScheduleRepository GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new WorkScheduleRepository();
+            }
+            return instance;
+        }
+        public List<WorkSchedule> GetWorkSchedules()
+        {
+            return workSchedules;
+        }
+    }
+}
---
+++ KoWorkersTests/WorkScheduleTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KoWorkers.WorkSchedule;
+using KoWorkers;
+
+namespace KoWorkersTests
+{
+    [TestClass]
+    public class WorkScheduleTest
+    {
+        Employee testEmployee = new Employee("Niels", "Hansen", 1234, 12345678);
+        DateTime thisMoment = DateTime.Now;
+        
+        [TestInitialize]
+        public void Initialize()
+        {
+        }
+        [TestMethod]
+        public void CreateNewShift()
+        {
+            WorkScheduleShift newShift = new WorkScheduleShift(testEmployee, thisMoment, thisMoment.AddHours(2));
+            TimeSpan timeSpan = new TimeSpan(2, 0, 0);
+            Assert.AreEqual("Niels Hansen", newShift.Employee.FullName);
+            Assert.AreEqual(timeSpan, newShift.EndTime.Subtract(newShift.StartTime));
+            Assert.AreEqual(120, newShift.TotalNumberOfMinutes);
+        }
+        [TestMethod]
+        public void CreateNewSchedule()
+        {
+            WorkScheduleShift newShift = new WorkScheduleShift(testEmployee, thisMoment, thisMoment.AddHours(2));
+            WorkSchedule testSchedule = new WorkSchedule(2018, 3);
+            Assert.AreEqual(2018, testSchedule.Year);
+            Assert.AreEqual(0, testSchedule.GetAllShifts().Count);
+            testSchedule.AddShiftToSchedule(newShift);
+            Assert.AreEqual(1, testSchedule.GetAllShifts().Count);
+        }
+    }
+}
