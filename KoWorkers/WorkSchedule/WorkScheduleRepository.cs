42fb8dc071378ba23e476c7ff83f322be40be1cd
jesp209i <jesp209i@edu.eal.dk>
2018-02-26 15:07:34 +0100

    kan hente alt lort

--- KoWorkers/WorkSchedule/WorkScheduleRepository.cs
+++ KoWorkers/WorkSchedule/WorkScheduleRepository.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace KoWorkers.WorkSchedule
 {
     public class WorkScheduleRepository
     {
+        private static string connectionString =
+  "server = EALSQL1.eal.local; database = DB2017_C02; user Id=USER_C02; Password=SesamLukOp_02;";
         private static WorkScheduleRepository instance = null;
         private List<WorkSchedule> workSchedules;
         private WorkScheduleRepository()
         {
             workSchedules = new List<WorkSchedule>();
+            GetAllWorkScheduleShifts();
         }
         public static WorkScheduleRepository GetInstance()
         {
@@ -52,6 +56,49 @@ namespace KoWorkers.WorkSchedule
         public void AddWorkSchedule(WorkSchedule newWorkSchedule)
         {
             workSchedules.Add(newWorkSchedule);
+        }
+        public void GetAllWorkScheduleShifts()
+        {
+            List<WorkScheduleShift> listOfAllShifts = new List<WorkScheduleShift>();
+            workSchedules.Clear();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+
+                    SqlCommand allShiftsFromDB = new SqlCommand("Sp_GetWorkScheduleShifts", con);
+                    allShiftsFromDB.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    SqlDataReader reader = allShiftsFromDB.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            int shiftId = int.Parse(reader["ShiftID"].ToString());
+                            int employeeId = int.Parse(reader["EmployeeID"].ToString());
+                            DateTime startTime = Convert.ToDateTime(reader["StartTime"]);
+                            DateTime endTime = Convert.ToDateTime(reader["EndTime"]);
+                            Employee employeeToShift = EmployeeRepository.GetInstance().GetEmployeeById(employeeId);
+                            WorkScheduleShift newShift = new WorkScheduleShift(shiftId, employeeToShift, startTime, endTime);
+                            listOfAllShifts.Add(newShift);
+                        }
+                    }
+                }
+                catch (SqlException e) { Console.WriteLine("Muuuuligvis en fejl\n" + e.Message); }
+            }
+            foreach (WorkScheduleShift wss in listOfAllShifts)
+            {
+                int shiftYear = int.Parse(wss.StartTime.ToString("yyyy"));
+                int shiftMonth = int.Parse(wss.StartTime.ToString("M"));
+                WorkSchedule workSchedule = this.GetSchedule(shiftYear, shiftMonth);
+                if (workSchedule == null)
+                {
+                    workSchedule = new WorkSchedule(shiftYear, shiftMonth);
+                    AddWorkSchedule(workSchedule);
+                }
+                workSchedule.AddShiftToSchedule(wss);
+            }
         }
     }
 }
